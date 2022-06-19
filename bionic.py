f2a6eff5810238dcdb53f5aa44adf98ea26aabe8
Sayan-404 <sayantbs@gmail.com>
2022-06-19 08:21:15 +0530

    added file upload

--- bionic.py
+++ bionic.py
@@ -1,18 +1,26 @@
 import sys
 from simple_colors import *
 
-words = input() 
-list = words.split()
 
-for i in list:
-    size = len(i)
 
-    if(size<=2):
-     midpoint=1
-    elif(size%2==0):
-        midpoint = size/2
-    else:
-        midpoint = (size+1) / 2
-    bolded = i[0:int(midpoint)]
-    others = i[int(midpoint):size]
-    print(green(bolded,['bold'])+green(others),end=" ")
+def convert(words):
+
+    list = words.split()
+
+    for i in list:
+        size = len(i)
+
+        if(size<=2):
+         midpoint=1
+        elif(size%2==0):
+            midpoint = size/2
+        else:
+            midpoint = (size+1) / 2
+        bolded = i[0:int(midpoint)]
+        others = i[int(midpoint):size]
+        print(green(bolded,['bold'])+green(others),end=" ")
+    print("")
+
+f = open("example.txt","r")
+for line in f:
+    convert(line)
